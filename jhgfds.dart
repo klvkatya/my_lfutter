e35f21504400bd888bc316d1a0828b4af9a4cf3a
klvkatya <kuleva0007@gmail.com>
2025-10-08 14:37:54 +0300

    Create jhgfds.dart

---
+++ jhgfds.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MyHomePage(title: 'Flutter Example App');
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 10;
+List<int> myList =[8];
+int points=0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter*=-1;
+      var some_card=Random().nextInt(11);
+      myList.add(some_card);
+      points+=some_card;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return MaterialApp(
+      title: 'Flutter App!!',
+      themeMode: _counter>0 ? ThemeMode.dark : ThemeMode.light,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.black,
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home:  Scaffold(
+      appBar: AppBar(
+ //       backgroundColor: _counter>0 ? Colors.red : Colors.blue,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+      
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+     if (_counter>0) Text('true'),
+      if (_counter<0) Text('false'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          Container(
+            width: 120,
+            height: 200,
+      color: Colors.lightBlueAccent,
+      alignment: Alignment.center,
+      child: Text(
+        myList.last.toString(),
+          textDirection: TextDirection.ltr
+        )
+      ),
+      Text(points.toString()),
+  
+          
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ));
+  }
+}
